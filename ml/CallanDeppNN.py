5a28cf9506081d0534c6d04d52e44f5e27c5c446
CallanLoomes <68879589+CallanLoomes@users.noreply.github.com>
2022-07-21 14:55:55 +1200

    Added Additional Cript for Deep Learning

--- ml/CallanDeppNN.py
+++ ml/CallanDeppNN.py
@@ -7,27 +7,24 @@ import pathlib
 from random import seed
 from random import randint
 
-import matplotlib.pyplot as plt
-
 #Limit the number of memory used for GPU
 print("TensorFlow version:", tf.__version__)
 GPU_Device = tf.config.experimental.list_physical_devices('GPU')
 print("Num GPUs Available: ", len(GPU_Device))
 tf.config.experimental.set_memory_growth(GPU_Device[0], True)
-#os.environ['TF_GPU_ALLOCATOR'] = 'cuda_malloc_async'
 
 #Go to the directory with the data
-data_dir = pathlib.Path("../DataGenerationUpdated/UserDataTraining/")
+data_dir = pathlib.Path("../DataGenerationUpdated/BaseData/")
 
 #Mark the dataset as the png images from the files
 list_ds = tf.data.Dataset.list_files(str(data_dir/"*/*.png"))
 
 #Indicate the negative dataset
-nosignal = list(data_dir.glob('UserNoSignal/*'))
+nosignal = list(data_dir.glob('NoSignal/*'))
 PIL.Image.open(str(nosignal[0]))
 
 #Determining Seed
-seed = 1234
+seed = 642524
 #seed = randint(0, 5000)
 print("The seed for this run is: ", seed)
 
@@ -66,8 +63,6 @@ model = tf.keras.Sequential([
   normalization_layer,
   tf.keras.layers.Conv2D(64, 3, activation='relu'),
   tf.keras.layers.MaxPooling2D(),
-  tf.keras.layers.Conv2D(32, 3, activation='relu'),
-  tf.keras.layers.MaxPooling2D(),
   tf.keras.layers.Flatten(),
   tf.keras.layers.Dense(128, activation='relu'),
   tf.keras.layers.Dropout(0.25),
@@ -79,28 +74,8 @@ model.compile(
   loss='binary_crossentropy',
   metrics=['accuracy'])
 
-history = model.fit(
+model.fit(
   train_ds,
   validation_data=validation_ds,
-  epochs=2,
+  epochs=10,
 )
-
-#Plotting results of model
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-
-plt.plot(epochs, acc, 'bo', label='Training acc')
-plt.plot(epochs, val_acc, 'b', label='Validation acc')
-plt.title('Training and validation accuracy')
-plt.legend()
-
-plt.figure()
-
-plt.plot(epochs, loss, 'bo', label='Training loss')
-plt.plot(epochs, val_loss, 'b', label='Validation loss')
-plt.title('Training and validation loss')
-plt.legend()
-
-plt.show()
