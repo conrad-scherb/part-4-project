ed6ecf790c3f1cdf999e714c3903e12569e8e12a
Conrad Scherb <68687721+conrad-scherb@users.noreply.github.com>
2022-10-02 13:18:30 +1300

    updates

---
+++ ml/visualisation/check-result.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+import numpy as np
+
+model = tf.keras.models.load_model("./ml/Model_1Neuron_13Kernel_0.89accuracy")
+
+img2 = load_img("./ml/visualisation/no-signal-horizontal.png", target_size=(128, 128))
+img = img_to_array(img2)
+img = np.expand_dims(img, axis=0);
+
+print(model.predict(img))
