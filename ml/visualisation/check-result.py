8ca9032f9c78593c1ccc841f4b62e10eb26fd357
Conrad Scherb <68687721+conrad-scherb@users.noreply.github.com>
2022-10-12 10:33:54 +1300

    updates

--- ml/visualisation/check-result.py
+++ ml/visualisation/check-result.py
@@ -1,10 +1,10 @@
 import tensorflow as tf
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import numpy as np
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
 model = tf.keras.models.load_model("./ml/Model_1Neuron_13Kernel_0.89accuracy")
 
-img2 = load_img("./ml/visualisation/horizontal-offset-signal.png", target_size=(128, 128))
+img2 = load_img("./DataGenerationUpdated/diagonal.png", target_size=(128, 128))
 img = img_to_array(img2)
 img = np.expand_dims(img, axis=0);
 
